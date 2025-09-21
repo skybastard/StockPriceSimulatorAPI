fe5f1f3066ec8aa8818cf4121efff086d2483e58
Martin Tamm <martin.a.tamm@ericsson.com>
2025-09-21 11:52:59 +0300

    Git to include the external projects

---
+++ CsvFormatter/CsvPriceFormatter.cs
@@ -0,0 +1,12 @@
+﻿using Contracts;
+
+namespace CsvFormatter
+{
+    public class CsvPriceFormatter : IDataFormatter
+    {
+        public string FormatPrice(string symbol, decimal price, DateTime timestamp)
+        {
+            return $"{symbol},{price},{timestamp:HH:mm:ss}";
+        }
+    }
+}
